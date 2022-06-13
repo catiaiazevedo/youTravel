d6fd7a17bb96f167bc5eb9a1a471f3348afabb31
catiaiazevedo <catiaiazevedo@ua.pt>
2022-06-13 10:12:31 +0100

    firestore retrieve, qr code and layout work done

--- app/src/main/java/com/example/youtravel/Location.java
+++ app/src/main/java/com/example/youtravel/Location.java
@@ -9,12 +9,16 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
@@ -26,13 +30,40 @@ import java.nio.charset.StandardCharsets;
 
 public class Location extends AppCompatActivity {
 
-    TextView title;
-    Button back;
+    TextView title, tag, price;
+    Button back, explore, home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        home = findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopup(view);
+            }
+        });
+
+        Bundle bundle = getIntent().getExtras();
+        String name = bundle.getString("name");
+        String image = bundle.getString("second_image");
+        String tagLine = bundle.getString("tagLine");
+        String prices = bundle.getString("price");
+        String description = bundle.getString("description");
+
+        explore = findViewById(R.id.explore);
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Exlore.class);
+                intent.putExtra("name", name);
+                intent.putExtra("description", description);
+                intent.putExtra("price", prices);
+                startActivity(intent);
+            }
+        });
 
         back = findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
@@ -41,16 +72,8 @@ public class Location extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 onPause();
-                finish();
             }
         });
-
-        Bundle bundle = getIntent().getExtras();
-        String name = bundle.getString("name");
-        String image = bundle.getString("second_image");
-
-        System.out.println(name);
-        System.out.println(image);
 
         LinearLayout layout =(LinearLayout) findViewById(R.id.layout);
 
@@ -76,6 +99,48 @@ public class Location extends AppCompatActivity {
 
         title = findViewById(R.id.name);
         title.setText(name);
+
+        tag = findViewById(R.id.tag);
+        tag.setText(tagLine);
+
+        price = findViewById(R.id.price);
+        price.setText("since "+prices);
+    }
+
+    private void logoutUser()
+    {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+        onPause();
+        finish();
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu, popup.getMenu());
+        popup.show();
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.addTrip:
+                        return true;
+
+                    case R.id.myTrips:
+                        return true;
+
+                    case R.id.logout:
+                        logoutUser();
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/youtravel/MainActivity.java
+++ app/src/main/java/com/example/youtravel/MainActivity.java
@@ -7,14 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,13 +49,26 @@ public class MainActivity extends AppCompatActivity {
     List<String> frontImages;
     List<String> names;
     List<String> secondImages;
+    List<String> tagLines;
+    List<String> price;
+    List<String> description;
     ImageView firstImage, secondImage, thirdImage, fourthImage, fifthImage, sixthImage, seventhImage;
     TextView firstText, secondText, thirdText, fourthText, fifthText, sixthText, seventhText;
+
+    Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        home = findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopup(view);
+            }
+        });
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = auth.getCurrentUser();
@@ -125,6 +149,128 @@ public class MainActivity extends AppCompatActivity {
                         fifthText.setText(names.get(4));
                         sixthText.setText(names.get(5));
                         seventhText.setText(names.get(6));
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, names);
+                        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.searchBar);
+                        textView.setAdapter(adapter);
+
+                        textView.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                            }
+
+                            @Override
+                            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                                adapter.getFilter().filter(charSequence);
+                            }
+
+                            @Override
+                            public void afterTextChanged(Editable editable) {
+
+                            }
+                        });
+                    }
+
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "No such document", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                else
+                {
+                    Toast.makeText(MainActivity.this, "get failed with "+task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void locationOnClick(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), Location.class);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("categories").document("todos");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    names = (List<String>) document.get("name");
+                    secondImages = (List<String>) document.get("second_image");
+                    tagLines = (List<String>) document.get("tagLine");
+                    price = (List<String>) document.get("price");
+                    description = (List<String>) document.get("description");
+
+                    if (document.exists())
+                    {
+                        switch (view.getId())
+                        {
+                            case R.id.firstImage:
+                                intent.putExtra("name", names.get(0));
+                                intent.putExtra("second_image", secondImages.get(0));
+                                intent.putExtra("tagLine", tagLines.get(0));
+                                intent.putExtra("price", price.get(0));
+                                intent.putExtra("description", description.get(0));
+                                startActivity(intent);
+                                onPause();
+                            break;
+                            case R.id.secondImage:
+                                intent.putExtra("name", names.get(1));
+                                intent.putExtra("second_image", secondImages.get(1));
+                                intent.putExtra("tagLine", tagLines.get(1));
+                                intent.putExtra("price", price.get(1));
+                                intent.putExtra("description", description.get(1));
+                                startActivity(intent);
+                                onPause();
+                            break;
+                            case R.id.thirdImage:
+                                intent.putExtra("name", names.get(2));
+                                intent.putExtra("second_image", secondImages.get(2));
+                                intent.putExtra("tagLine", tagLines.get(2));
+                                intent.putExtra("price", price.get(2));
+                                intent.putExtra("description", description.get(2));
+                                startActivity(intent);
+                                onPause();
+                            break;
+                            case R.id.fourthImage:
+                                intent.putExtra("name", names.get(3));
+                                intent.putExtra("second_image", secondImages.get(3));
+                                intent.putExtra("tagLine", tagLines.get(3));
+                                intent.putExtra("price", price.get(3));
+                                intent.putExtra("description", description.get(3));
+                                startActivity(intent);
+                                onPause();
+                            break;
+                            case R.id.fifthImage:
+                                intent.putExtra("name", names.get(4));
+                                intent.putExtra("second_image", secondImages.get(4));
+                                intent.putExtra("tagLine", tagLines.get(4));
+                                intent.putExtra("price", price.get(4));
+                                intent.putExtra("description", description.get(4));
+                                startActivity(intent);
+                                onPause();
+                            break;
+                            case R.id.sixthImage:
+                                intent.putExtra("name", names.get(5));
+                                intent.putExtra("second_image", secondImages.get(5));
+                                intent.putExtra("tagLine", tagLines.get(5));
+                                intent.putExtra("price", price.get(5));
+                                intent.putExtra("description", description.get(5));
+                                startActivity(intent);
+                                onPause();
+                            break;
+                            case R.id.seventhImage:
+                                intent.putExtra("name", names.get(6));
+                                intent.putExtra("second_image", secondImages.get(6));
+                                intent.putExtra("tagLine", tagLines.get(6));
+                                intent.putExtra("price", price.get(6));
+                                intent.putExtra("description", description.get(6));
+                                startActivity(intent);
+                                onPause();
+                            break;
+                        }
                     }
 
                     else
@@ -150,84 +296,28 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    public void locationOnClick(View view)
-    {
-        Intent intent = new Intent(getApplicationContext(), Location.class);
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu, popup.getMenu());
+        popup.show();
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference docRef = db.collection("categories").document("todos");
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    names = (List<String>) document.get("name");
-                    secondImages = (List<String>) document.get("second_image");
-                    if (document.exists())
-                    {
-                        switch (view.getId())
-                        {
-                            case R.id.firstImage:
-                                intent.putExtra("name", names.get(0));
-                                intent.putExtra("second_image", secondImages.get(0));
-                                startActivity(intent);
-                                onPause();
-                                finish();
-                            break;
-                            case R.id.secondImage:
-                                intent.putExtra("name", names.get(1));
-                                intent.putExtra("second_image", secondImages.get(1));
-                                startActivity(intent);
-                                onPause();
-                                finish();
-                            break;
-                            case R.id.thirdImage:
-                                intent.putExtra("name", names.get(2));
-                                intent.putExtra("second_image", secondImages.get(2));
-                                startActivity(intent);
-                                onPause();
-                                finish();
-                            break;
-                            case R.id.fourthImage:
-                                intent.putExtra("name", names.get(3));
-                                intent.putExtra("second_image", secondImages.get(3));
-                                startActivity(intent);
-                                onPause();
-                                finish();
-                            break;
-                            case R.id.fifthImage:
-                                intent.putExtra("name", names.get(4));
-                                intent.putExtra("second_image", secondImages.get(4));
-                                startActivity(intent);
-                                onPause();
-                                finish();
-                            break;
-                            case R.id.sixthImage:
-                                intent.putExtra("name", names.get(5));
-                                intent.putExtra("second_image", secondImages.get(5));
-                                startActivity(intent);
-                                onPause();
-                                finish();
-                            break;
-                            case R.id.seventhImage:
-                                intent.putExtra("name", names.get(6));
-                                intent.putExtra("second_image", secondImages.get(6));
-                                startActivity(intent);
-                                onPause();
-                                finish();
-                            break;
-                        }
-                    }
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.addTrip:
+                        return true;
 
-                    else
-                    {
-                        Toast.makeText(MainActivity.this, "No such document", Toast.LENGTH_SHORT).show();
-                    }
-                }
+                    case R.id.myTrips:
+                        return true;
 
-                else
-                {
-                    Toast.makeText(MainActivity.this, "get failed with "+task.getException(), Toast.LENGTH_SHORT).show();
+                    case R.id.logout:
+                        logoutUser();
+                        return true;
+
+                    default:
+                        return false;
                 }
             }
         });
